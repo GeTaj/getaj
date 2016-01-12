5084a8c9d61f6ddf575d6e15d859d356d8ca0688
zhupeng <416175331@qq.com>
2016-01-12 10:50:03 +0800

    angularJS

---
+++ js/controller.js
@@ -0,0 +1,43 @@
+function PhoneListCtrl($scope){
+	$scope.lines = [
+		{"name": "Nexus S",
+		 "info": "Fast just got faster with Nexus S."},
+		{"name": "Motorola XOOM™ with Wi-Fi",
+		 "info": "The Next, Next Generation tablet."},
+		{"name": "MOTOROLA XOOM™",
+		 "info": "The Next, Next Generation tablet."}
+		  ];
+
+}
+function LineListCtrl($scope){
+	$scope.lines = [
+		{"name": "Nasdadasdasdasdasdasdasdasd",
+		 "info": "Fas."},
+		{"name": "Motorola XOOM™ with Wi-Fi",
+		 "info": "Theration tablet."},
+		{"name": "aaaaaaaaaaaaaaaaaaaaaaaaaa",
+		 "info": "bbbbbbbbbbbbbbbb"},
+		{"name": "ccccc",
+		 "info": "dddd"},
+		{"name": "eeee",
+		 "info": "ffff"},
+		{"name": "ggg",
+		 "info": "hhh"},
+		{"name": "iii",
+		 "info": "jjj"},
+		{"name": "kkk",
+		 "info": "lll"},
+		{"name": "MOTOROLA XOOM™",
+		 "info": "Tlet."}
+		  ];
+}
+
+
+
+
+
+
+
+
+
+
